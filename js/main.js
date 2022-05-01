6a408727becb63a3814970f637792b166b107640
ikerlunacruz <lunacruziker22@gmail.com>
2022-05-02 00:59:13 +0200

    first comm

--- js/main.js
+++ js/main.js
@@ -1,8 +1,25 @@
-function parrafos() {
-    var infonum = document.getElementById('infonum')
-    infonum.innerHTML = 'You entered a non binary digit (please enter only 0 or 1)'
-    var infonum2 = document.getElementById('infonum2')
-    if (numero << 1) {
-        var infonum2 = 'Here is your decimal'
+function texto() {
+    var iker1 = document.getElementById('iker1')
+    var iker2 = document.getElementById('iker2')
+    var numerobi = document.getElementById('numerobi').value
+    var procesando = document.getElementById('procesando')
+    procesando.innerHTML = parseInt(numerobi, 2);
+    var ultimodigito = numerobi.charAt(numerobi.length - 1)
+    if (numerobi == '') {
+        procesando.innerHTML = ''
+    }
+    if (ultimodigito == 0 || ultimodigito == 1) {
+        infonum2.innerHTML = 'Here is your decimal'
+        infonum.innerHTML = ''
+    } else {
+        procesando.innerHTML = 'Waiting for a valid binary number...'
+    }
+    if (ultimodigito >> 1 || ultimodigito >> 9999) {
+        infonum.innerHTML = 'You entered a non binary digit (please enter only 0 or 1)'
+        infonum2.innerHTML = ''
+    }
+    if (ultimodigito == '') {
+        infonum.innerHTML = ''
+        infonum2.innerHTML = ''
     }
 }
